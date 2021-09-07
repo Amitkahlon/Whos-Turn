fe532f1a0594e57d9da218fe247311c35c2ca86d
Amit Kahlon <amitkahlon4321@gmail.com>
2021-09-07 19:04:30 +0300

    dev init

---
+++ src/components/baseComponents/BaseTimePicker.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const BaseTimePicker = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default BaseTimePicker
+
+const styles = StyleSheet.create({})
